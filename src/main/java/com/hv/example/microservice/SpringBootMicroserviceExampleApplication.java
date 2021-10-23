e9b1a12f2b35adf1e722d26469a19d8803f61afc
Jonathan Hernandez <hv12i04001@gmail.com>
2021-10-23 12:20:28 +0200

    initial project

---
+++ src/main/java/com/hv/example/microservice/SpringBootMicroserviceExampleApplication.java
@@ -0,0 +1,13 @@
+package com.hv.example.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hv/example/microservice/SpringBootMicroserviceExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hv.example.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroserviceExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
