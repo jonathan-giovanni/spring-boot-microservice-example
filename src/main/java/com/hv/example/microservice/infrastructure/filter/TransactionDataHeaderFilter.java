58b0231ab7f56453fb6d16a1b165666c1f2c52ae
Jonathan Hernandez <hv12i04001@gmail.com>
2021-10-23 15:22:39 +0200

    skipped actuator health

--- src/main/java/com/hv/example/microservice/infrastructure/filter/TransactionDataHeaderFilter.java
+++ src/main/java/com/hv/example/microservice/infrastructure/filter/TransactionDataHeaderFilter.java
@@ -1,6 +1,8 @@
 package com.hv.example.microservice.infrastructure.filter;
 
+import com.hv.example.microservice.infrastructure.logging.LogCategory;
 import com.hv.example.microservice.infrastructure.logging.TransactionUtil;
+import com.hv.example.microservice.infrastructure.logging.WSLog;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.ContentCachingRequestWrapper;
@@ -15,18 +17,31 @@ import java.io.IOException;
 @Component
 public class TransactionDataHeaderFilter extends OncePerRequestFilter {
 
+    private final WSLog wsLog;
+
+    public TransactionDataHeaderFilter(WSLog wsLog) {
+        this.wsLog = wsLog;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
         ContentCachingRequestWrapper wrappedRequest     = new ContentCachingRequestWrapper(((HttpServletRequest) request));
 
         TransactionUtil.begin(wrappedRequest);
-
         TransactionUtil.timerStart();
+
+        if(wrappedRequest.getRequestURI().contains("actuator")){
+            TransactionUtil.setLogSkipped(true);
+        }
+
+        wsLog.info(LogCategory.FILTER,"REQUEST: "+request.getRequestURI());
 
         HttpServletResponse wrappedResponse = new HttpServletResponseWrapper(response) {
             @Override
             public void setStatus(int sc) {
                 super.setStatus(sc);
+
+                wsLog.info(LogCategory.FILTER,"HTTP STATUS: "+sc);
 
                 TransactionUtil.timerStop();
                 TransactionUtil.clear(this);
